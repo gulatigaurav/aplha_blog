7ce4a8c203f0f2cb526b9a606ec29aae24df8478
Gaurav Rails <gauravgulati9c@gmail.com>
2017-07-08 23:26:37 +0530

    Create , Show and Display error messages if any

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,19 @@ def new
 end
 
 def create
-  # render plain: params[:article].inspect
   @article = Article.new(article_params)
-  @article.save
-  redirect_to articles_show(@article)
+   if @article.save
+          flash[:notice]= "Article was successfully created"
+          redirect_to article_path(@article)
+   else
+    #  flash[:alert] = @article.errors.full_messages
+     render 'new'
+   end
 # we need to white list the articles params
+end
+
+def show
+@article = Article.find(params[:id])
 end
 
 private
