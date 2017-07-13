4b8a22d8193572a293c2307ec913987f9af44351
Gaurav Rails <gauravgulati9c@gmail.com>
2017-07-13 18:36:24 +0530

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
+
 before_action  :set_article , only: [:edit,:update,:destroy,:show]
+before_action :require_user, except: [:index, :show]
+before_action :require_same_user, only: [:edit, :update, :destroy]
 def index
   @articles = Article.paginate(page: params[:page],per_page: 5)
 end
@@ -11,7 +14,7 @@ end
 def create
   debugger
   @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
    if @article.save
           flash[:success]= "Article was successfully created"
           redirect_to article_path(@article)
@@ -46,6 +49,14 @@ def update
 end
 
 private
+
+def require_same_user
+if current_user != @article.user
+  flash[:danger] = "You can only edit or delete your own articles"
+  redirect_to root_path
+end
+end
+
 
  def set_article
    @article = Article.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ resources :articles
   get '/about', to: 'pages#about'
 get '/signup',  to: 'users#new'
 
- resources :users, except: [:new]
+get '/login',  to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
+resources :users, except: [:new]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
