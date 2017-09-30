f2730b0c40bb315cf53a72aa779267f56721e349
Gaurav Gulati <gauravgulati9c@gmail.com>
2017-09-30 06:53:41 +0530

    Add some validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,6 @@
      has_many :comments, dependent: :destroy
      has_many :categories, through: :article_categories
      validates :title, presence: true , length: {minimum: 3, maximum: 50}
-     validates :description, presence: true, length: {minimum: 10,maximum: 300}
+     validates :description, presence: true, length: {minimum: 10,maximum: 5000}
      validates :user_id , presence: true
 end
